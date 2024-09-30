3947ac701de768bc5a25d448cb6c0e857ba90a8e
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2024-09-30 19:09:48 +0200

    Initial commit

---
+++ Assets/Scripts/abstract_clases/CharacterCard.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCard : Card
+{
+    public Character: characterSO;
+    public static Invoke
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
